1b180a63dcf8654b2fa98a89b2a9df18b6ff20a9
mohitkumarmaheshwari <144377618+mohitkumarmaheshwari@users.noreply.github.com>
2023-11-02 11:58:33 +0530

    Update app (8).py

--- app (8).py
+++ app (8).py
@@ -1,43 +1,32 @@
 #6. Build a Flask app that allows users to upload files and display them on the website.
+from flask import Flask, request, render_template
 import os
-from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Folder where uploaded files will be stored
 
-# Set the upload folder and allowed extensions
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Ensure the 'uploads' directory exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-# Function to check if the file has an allowed extension
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# Define a route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Check if the POST request has a file part
+        # Check if the post request has a file part
         if 'file' not in request.files:
-            return redirect(request.url)
+            return render_template('index.html', error='No file part')
         
         file = request.files['file']
         
-        # If the user does not select a file, browser submits an empty file without a filename
+        # If the user does not select a file, browser also submits an empty file
         if file.filename == '':
-            return redirect(request.url)
-
-        # If the file is allowed, save it and redirect to the home page
-        if file and allowed_file(file.filename):
+            return render_template('index.html', error='No selected file')
+        
+        if file:
             filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filename)
-            return redirect('/')
+            return render_template('index.html', success='File uploaded successfully', filename=file.filename)
     
-    # List the files in the uploads folder
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    
-    # Render the template with the list of uploaded files
-    return render_template('index.html', files=files)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True,port=8088)
+    app.run(host="0.0.0.0",debug=True,port=8080)
